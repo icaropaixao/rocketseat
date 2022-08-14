d93cf4c7bbc0e556457dadeae844944ec6914c58
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2022-08-14 19:12:58 -0300

    Scope block statement

---
+++ java-script/variaveis/variavel3.js
@@ -0,0 +1,21 @@
+/*
+ ESCOPO determina a visibilidade
+ de alguma variável no JS
+*/
+
+// BLOCK STATEMENT
+
+// tudo que está dentro da tag script é o escopo GLOBAl
+console.log("> existe x antes do bloco?",x)
+
+
+/*
+ tudo que esta dentro das chaves
+ é um escopo local
+*/
+{
+    let x= 0
+    console.log(x)
+}
+
+console.log("> existe x depois do bloco?",x)
