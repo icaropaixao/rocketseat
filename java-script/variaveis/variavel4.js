b6203aad5b3bdce8a9c8634be58294c7dafab6c9
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2022-08-14 20:26:49 -0300

    Var Names Exemples

---
+++ java-script/variaveis/variavel4.js
@@ -0,0 +1,26 @@
+
+/*
+   Nomes de Variaveis Regras
+*/
+
+/* * Pode iniciar com:
+    - Iniciar com caracteres especias: $
+    - iniciar com letras 
+    - colocar acentos
+    - Letras maíusculas e minúsculas fazem difereça
+*/
+
+/* * Não pode iniciar com:
+    - Iniciar com números   
+    - colocar espaços vazios no nome
+*/
+
+/* *  Forma Ideal:
+    - Criar nomes que fazem sentido
+    - Que explique o que a variavel é ou faz
+    - Casel Case exemplo: VariavelExemplo
+    - escrever em inglês
+*/
+let $Name= "icaro"
+console.log($Name)
+ 
