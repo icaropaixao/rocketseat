7164524ecc5e4e06be9b307a2f7f30ff144871de
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2022-08-14 17:16:32 -0300

    Variavel in JavaScript

---
+++ java-script/variaveis/definição.md
@@ -0,0 +1,9 @@
+# variaveis
+
+* Nomes simbólico para receber um valor
+* Atalhos de código
+
+* 3 paçavras reservadas para criar uma variavel
+  - Var
+  - Let
+  - Const
---
+++ java-script/variaveis/variavel1.js
@@ -0,0 +1,23 @@
+
+//exemplo de variavel
+
+var dia= "quente"
+dia= "nublado"
+console.log(dia)
+
+
+/* 
+  variavel tipo "LET" ou "VAR"
+  podem receber mais de um valor!!
+*/
+let sol= "quente"
+sol= "frio"
+console.log(sol)
+
+
+/*
+ Variavel tipo "CONST"
+ pode receber apenas um único valor!
+ */
+const clima="quente"
+console.log(clima)
