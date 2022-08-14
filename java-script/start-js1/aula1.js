82192c134686cedba8055db606d1b3250a842843
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2022-08-14 10:57:35 -0300

    JavaScript start 1

---
+++ java-script/start-js1/aula1.js
@@ -0,0 +1,10 @@
+
+console.log("icaro alcançou a ascensão")
+
+// para usar um comentário
+// basta usar duas barras na linha e pronto xD
+
+/*
+aqui tenho um comentário com mais de uma linha!!!
+*/
+
