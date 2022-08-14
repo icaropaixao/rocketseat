7baa63a6e8a453dc721b0c95087dec71afd01eb6
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2022-08-14 12:36:01 -0300

    Undefined And Null in JS

---
+++ java-script/conceitos-basicos/undefined-null.js
@@ -0,0 +1,12 @@
+
+//No JavaScript temos o tipo de dados
+
+ /*undefined 
+   -representa valores indefinidos*/
+   console.log(undefined)
+
+ /*null
+   -nulo
+   -objeto que não possui nada dentro
+   - diferente de indefinido */
+   console.log(null)
