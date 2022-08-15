27f0bc4069c74b21128557dc609b94374f4a5b97
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2022-08-15 19:29:28 -0300

    Template String in JS

---
+++ java-script/variaveis/var2-exemplos.js
@@ -0,0 +1,14 @@
+
+// Objectc
+
+const person={
+    name:'icaro',
+    age: 18,
+    weight:70.2,
+    isAdmin: true
+}
+//pegar um valor do object
+console.log(person.name)
+
+// interpolação em object ou concatenação
+console.log(`${person.name} tem ${person.age} anos`)
