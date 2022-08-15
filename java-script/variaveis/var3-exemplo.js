0df5678658a6553cc49fdc76f3a0efba7c15d974
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2022-08-15 19:48:07 -0300

    Arrays

---
+++ java-script/variaveis/var3-exemplo.js
@@ -0,0 +1,24 @@
+
+// Arrays
+
+
+//defindo o vetor
+const animals=[
+    "lion",
+    "cat",
+    "monkey"
+]
+
+/*
+ para acessar os valores do array
+ usa-se (nome do var[0]) sempre começa por 0
+ a sequencia de cadeia dos vetores
+ */
+console.log(animals[0])
+
+
+/*
+ para descobrir o numero de posições do vetor
+ usa-se o (.lenght)
+ */ 
+console.log(animals.length)
