c30319ad7b32a8d8de7d4e165625561ddfbf4ce3
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2022-08-16 21:07:09 -0300

    Functions in JavaScript

---
+++ java-script/funções/functions1.js
@@ -0,0 +1,13 @@
+// criar um aplicativo de frases motivacionais
+
+// declaração da função(function declaration)
+function createPhrases() {
+    console.log("Estudar é muito bom")
+    console.log("Paciència e persintência")
+    console.log("Revisão é mãe do aprendizado")
+}
+
+// executando a funçaõ, rodar, invocar...
+createPhrases()
+
+console.log("Fim do progama")
---
+++ java-script/funções/functions2.js
@@ -0,0 +1,9 @@
+// function expression
+// função dentro de uma variavel
+
+// parametros (parameters)
+const sum = function(number1, number2){
+    console.log(number1 + number2)
+}
+
+sum(2, 3) // arguments (argumentos)
