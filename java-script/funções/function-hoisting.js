c78060284381282044509ba75f55138307e11943
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2022-08-18 19:03:15 -0300

    Hoisting Function

---
+++ java-script/funções/function-hoisting.js
@@ -0,0 +1,12 @@
+// function hoisting
+
+
+/*
+ criando a funçaõ dessa forma
+ a função sofre o Hointing, que é como se o bloco de codigo fosse jogado pra cima e reorganizado
+ */ 
+sayMyName();
+
+function sayMyName(){
+    console.log("icaro")
+}
