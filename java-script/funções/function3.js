174ee70ea0f5fe4ca0601d5bc9780afda4289518
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2022-08-17 21:39:26 -0300

    Return in Functions

---
+++ java-script/funções/function3.js
@@ -0,0 +1,12 @@
+// Função é um liquidificador
+
+
+// exemplo de funçao 
+// exemplo de  return de valor para uma funçaõ
+function fazerSuco(fruta1, fruta2){
+    return "suco de :" + fruta1 + fruta2
+}
+
+const copo = fazerSuco("banana", "maça")
+
+console.log(copo)
