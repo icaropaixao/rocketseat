4bc3cc5dc71fb6acb457cfeca6ce2085bb7f8ea0
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2022-08-21 19:04:00 -0300

    Casas Decimais

---
+++ java-script/prototype/casas-decimais.js
@@ -0,0 +1,8 @@
+// Manipulação Strings e Numeros 
+
+/* tranformar um numero quebrado com duas casas decimais
+   e trocar ponto por virgula
+*/
+
+let number= 12213.2324
+console.log(number.toFixed(2).replace(".",","))
