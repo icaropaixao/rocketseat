7aaa942391e06509625d1bd78e025ad96e65a779
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2022-08-21 19:43:20 -0300

    Encontrando palavras em frases

---
+++ java-script/prototype/palavras-chave.js
@@ -0,0 +1,10 @@
+// Manipulação Strings e Numeros 
+
+
+/*
+  Encontrando palavras chaves em um texto
+  exemplo: amor
+*/
+
+let phrase= "eu quero Viver"
+console.log(phrase.includes("amor")) // detecta se é true ou false
