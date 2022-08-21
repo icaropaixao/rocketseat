34b3be9877c9e8350f101375b38ad6a080c3c9df
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2022-08-21 18:40:59 -0300

    Manipulation from types

---
+++ java-script/prototype/strings-numeros.js
@@ -0,0 +1,11 @@
+// Manipulando Strings e Numeros
+
+// Transformar String em Numero e Numero em String
+
+// string em numero
+let string= "123"
+console.log(Number(string))
+
+//Numero em string
+let number= 321
+console.log(String(number))
