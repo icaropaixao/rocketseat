7fba95398b1cd3148e0a4204bc86b7fca9cfbad0
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2022-08-21 18:29:47 -0300

    Type casting in JavaScript

---
+++ java-script/prototype/type-coersion.js
@@ -0,0 +1,10 @@
+/*
+  Tyoe conversion (typecasting) VS Type coersion
+
+  * Aleteração de um tipo de dado para outro tipo
+*/
+
+
+// exemplo de alteração de dado
+// forcei o "9" a virar um type Number ao inves de string
+ console.log(Number('9') + 5)
