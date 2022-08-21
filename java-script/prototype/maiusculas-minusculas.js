80d1c704d51d9b761fdb5c4ce46d39968395c307
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2022-08-21 19:10:28 -0300

    Maiúsculas e minúsculas

---
+++ java-script/prototype/maiusculas-minusculas.js
@@ -0,0 +1,14 @@
+// Manipulação Strings e Numeros 
+
+/*
+  Tranforme Letras minusculas em maiusculas
+  Faça o contrario disso tambem
+*/
+
+// minusculas
+let word= "progamar é bacana"
+console.log(word.toLowerCase())
+
+//maisculas
+let seila= "progamar é show de bola"
+console.log(seila.toUpperCase())
