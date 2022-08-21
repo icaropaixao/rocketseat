30e9acf6d0810e862145ffa7a45e961082d92bad
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2022-08-21 18:46:26 -0300

    Contando caracteres e digitos (Length)

---
+++ java-script/prototype/lenght.js
@@ -0,0 +1,15 @@
+// Manipulação Strings e Numeros 
+
+/*
+ Contar quantos caracteres tem uma palavra
+ e quantos digitos tem um numero
+*/
+
+// contando caracteres em um tipo string
+let word = "paralelepipedo"
+console.log(word.length)
+
+// contando caracteres em um tipo Number
+
+let number= 1234
+console.log(String(number).length)
