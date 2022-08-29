af5898922032e39dbfbfefd6a0d5219eb3885f2a
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2022-08-29 20:09:23 -0300

    arithmetic operators

---
+++ java-script/expressoes/script4.js
@@ -0,0 +1,23 @@
+/*
+ Expressoes aritmeticas
+*/
+
+/* resto da divisão: sinal % */
+let remainder
+remainder = 11 % 9
+console.log(remainder)
+
+/* incremento: sinal ++ */
+let increment = 0
+
+console.log(++increment)
+console.log(increment)
+
+
+/* decremento: sinal -- */
+let decrement = 0
+decrement--
+console.log(decrement)
+
+/* exponencial: sinal ** */ // potencia
+console.log(2 ** 3)
