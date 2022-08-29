12fe9ebc311a884597fcda824114c4b275c5e711
icaropaixao <106492577+icaropaixao@users.noreply.github.com>
2022-08-29 19:41:53 -0300

    NEW in Js

---
+++ java-script/expressoes/script2.js
@@ -0,0 +1,12 @@
+/*
+   New
+
+   *  left-hand-side expression
+   * criar um novo objeto 
+*/
+ 
+  let name = new String('Icaro')
+  name.surName = "Reis"
+  let age = new Number(18)
+
+  console.log(name, age)
